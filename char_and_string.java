012069cef8d377b71042bd026329f5b9af9fdf00
LorenzoCesarini <cesarinilorenzo.github@hotmail.com>
2015-03-08 16:03:40 +0100

    Added three Java programs which show how to print on screen  and how to handle integers, chars and strings

---
+++ char_and_string.java
@@ -0,0 +1,185 @@
+
+// Program Number 3
+// In this program we declare various char and string variables and  print them
+// LAST MODIFIED 08/03/2015
+
+
+
+class char_and_string {       // INITIALLY I  HAD CALLED THIS PROGRAM char BUT SINCE IT IS A TYPE IDENTIFIER IT IS ILLEGAL TO DO SO 
+
+public static void main(String[] arguments){
+
+
+
+//FIRST PART : CHARS
+
+char firstchar;   // this is the way to declare char variables
+
+firstchar='a';    // chars must be assigned using the single '.' quotation marks   
+
+
+// METHODS TO PRINT A CHAR
+
+System.out.println("This is the content of the char we have declared: "+firstchar); // ok it prints a correctly
+
+//second method : USING %c
+
+System.out.format("The content of the char firstchar is %c and everyone is happy\n" ,firstchar); //OK
+
+
+
+
+
+
+
+
+
+// SECOND PART: STRINGS
+
+String name;      // Here we declare a variable called name of String type, the String type starts always
+                  //  with a capital letter
+
+name="Aaaaa Bbbbb"; //  A STRING CAN BE DECLARED ONLY WITH THE DOUBLE QUOTATION MARKS "  ".
+                     // USING THE SINGLE QUOTATION MARKS '  ' LEADS TO AN ERROR   
+
+//HOW TO PRINT A STRING
+					 
+System.out.println("The content of the string name is:"+name);					 
+
+//how to print a string with %s
+
+System.out.format("My name is %s, hello everyone\n",name);					 
+
+
+					 
+					 
+// HOW TO MEASURE THE LENGHT OF A STRING
+
+int len= name.length();     // the command .length() allows to measure the lenght of the string. 
+
+System.out.println("the lenght of the string name is "+len);
+
+
+// HOW TO ACCESS THE CHARACTER IN A CERTAIN POSITION 
+
+char LETTER=name.charAt(4);   // the variable LETTER  will store the value: a
+                              // the char in the fourth position is actually the fifth letter
+                               //the count starts from zero!!!!!!!  							  
+System.out.println(LETTER);
+
+
+char FIRSTLETTER=name.charAt(0);  
+
+
+System.out.println("The first letter is "+FIRSTLETTER);  // this prints the first letter ,A, which is in the zeroth position
+
+
+// HOW TO MODIFY THE CHARACTER IN A CERTAIN POSITION
+//let us change the fifth letter
+
+//name.charAt(4)='C';
+//System.out.println("The modified name now is" +name );        // THESE TWO LINES DON'T WORK 
+
+
+//HOW TO ACCESS A SUBSTRING
+
+String firstname=name.substring(0,5);
+System.out.println("The first  name  is "  +firstname); // it prints only Aaaa
+
+String secondname=name.substring(6,11);
+System.out.println("The first  name  is " +secondname); // it prints only Bbbb
+
+
+
+
+
+// HOW TO DETERMINE THE POSITION OF A CERTAIN CHAR IN A STRING
+
+String greeting="Hello!";
+// we want to determine the position of the letter 'e' in 'Hello!' ,we expect to obtain 1 since the index counter starts from zero  
+int position=greeting.indexOf('e');
+
+System.out.println(position); // it prints correctly 1 OK
+
+//what happens if a string contains the same letter multiple times?
+//For example there are four a in name, what would happen then?  
+
+int position2=name.indexOf('a');
+
+System.out.println(position2);  //it prints only the index of the first hit
+ 
+
+ 
+ 
+// HOW TO DETERMINE THE INDEX AT THE BEGINNING OF A SUBSTRING   
+
+String text="Hello, this is just random junk typed by a monkey";
+// we here want to know the index of the first letter of the word random
+
+int pos=text.indexOf("random");  // NOTCE HERE THAT YOU MUST USE DOUBLE QUOTATION MARKS: "...."
+ 
+System.out.println(pos); // it prints 20 OK
+
+
+
+// HOW TO CONVERT A STRING TO UPPERCASE
+
+String newtext=text.toUpperCase();
+System.out.println(newtext);  // the entrire string text is printed in uppercase OK 
+
+
+//is it possible to convert a substring to uppercase? 
+
+String newstring=text.substring(20,26).toUpperCase();  // this shows that multiple features can be accessed using the .
+                                                // m has the index 25, we wrote 26 beacuse the last index in the interval is not included
+System.out.println(newstring);  //YES it prints RANDOM in uppercase OK
+
+
+
+// HOW TO ACCESS A SUBSTRING KNOWING ONLY ITS FIRST AND LAST LETTERS (both letters must be the first occurrences of certain chars)
+
+String text2="Hello, this is just random junk typed by a monkey";
+// imagine you have a huge string and want the substring wich begins with the first ocurrence of the letter 'r'
+//and ends with the first occurence of the letter 'm'
+
+String specialstring=text2.substring(text2.indexOf('r'),text2.indexOf('m')+1);
+System.out.println("This time the string is: "+specialstring); // it prints correctly random
+ 					 
+
+					 
+//HOW TO JOIN MULTIPLE STRINGS
+					 
+String one="HELLO";
+					 
+String two= " WORLD!";
+
+String three= one+two;
+
+System.out.println(three); // It prints correctly: HELLO WORLD!
+
+// STRINGS CAN BE SIMPLY JOINED BY ADDING THEM (THE SAME HAPPENS IN PYTHON)
+
+
+
+
+// HOW TO MODIFY THE CHARACTER IN A CERTAIN POSITION USING SUBSTRINGS
+//IN JAVA STRINGS CANNOT BE MODIFIED, WE NEED A NEW STRING:					 
+String oldname="Arnold Schwarzenegger";					 
+String newname=oldname.substring(0,13)+'s'+oldname.substring(14,21); 					 
+System.out.println(newname);  //we changed the z into an s					 
+
+
+
+
+
+
+					 
+}
+}
+
+
+
+
+
+
+
---
+++ integers.java
@@ -0,0 +1,167 @@
+
+// Program Number 2
+// In this program we declare and assign various int variables and  print them
+// LAST MODIFIED 08/03/2015
+
+class integers{
+
+public static void main(String[] arg){
+
+// USING INTEGERS AND PRINTING THEM
+
+int num;     // The int type starts always with a lower case letter
+
+num=6;       // AS WE CAN SEE THE DECLARATION OF A VARIABLE AND THE ASSIGNMENT OF ITS VALUE CAN HAPPEN IN TWO DIFFERENT MOMENTS
+
+System.out.println(num);  // this is the command to use if we want to print only the value of the variable
+
+System.out.println("The variable num has been assigned the value: "+num);  // to print a variable after the text you need to write the plus
+                                                                          // In Python and C instead a simple comma is needed
+
+																		  
+System.out.println("The variable num has been assigned the value "+num+" and we are happy like that"); 
+// this line shows how to print a variable between two statements.
+// Remember to leave a space both on the left and right of the quotation marks near the variable that you want to print
+
+
+// HOW TO PRINT MORE VARIABLES:
+
+int newnum =15;   //we need first to declare a new variable and assign a value to it 
+
+System.out.println("First Attempt: The variables num  and newnum have been assigned the values: "+ num + newnum);  //in this way the number printed is 615 we need spaces!!
+
+System.out.println("Second Attempt: The variables num  and newnum have been assigned the values: " + num+ "\t"+ newnum); 
+//  the tab  works but this is more complex than other programming languages SEE BELOW
+
+// third wrong attempt
+System.out.format("Third attempt: The variables num and newnum have been assigned the values: ", num, newnum); //prints only the text
+System.out.format("\n\n");   // and it doesen't go to a newline that's why I had to print two newline commands  
+ 
+
+
+// the correct way is the following:
+System.out.format("Fourth attempt: The variables num and newnum have been assigned the values: %d  %d \n", num, newnum);
+
+
+//what if we wanted to print only one variable?
+System.out.format("Fourth attempt: The variables num and newnum have been assigned the values: %d", num);
+// this is the unique working way if you use .format . In this case you must use also %d and the comma 
+// NOTICE THAT System.out.println IS MEANT TO PRINT TEXT LINES, THAT'S WHY WE USE THE +, IT IS THE COMMAND TO JOIN DIFFERENT STRINGS!!!!
+// System.out.format INSTEAD  MUST BE USED TO PRINT NUMBERS THAT WE WANT TO PERCEIVED FOR WHAT THEY ARE AND NOT AS STRINGS
+
+
+
+
+//IS IT POSSIBLE TO CARRY OUT OPERATIONS BETWEEN VARIABLES INSIDE THE PRINT? LET'S CHECK 
+
+System.out.println((num + newnum));  // this line prints correctly 21 : you need the double brackets, NO! see next line
+System.out.println(num+newnum);      // This line prints correctly 21 : DOUBLE BRACKETS ARE NOT NEEDED IF THERE ISN'T TEXT
+
+//let us try to print some text and carry an operation at the same time:
+
+System.out.println("The sum of the variables num and newnum is :"+(num + newnum)); 
+//this line works correctly, notice the difference between the two +: the first plays the role of the comma in C, the second is needed to add the two integers 
+
+
+
+// HOW TO REASSIGN A VALUE TO A VARIABLE
+
+int a=12;
+System.out.println("a="+a);
+a=33;
+System.out.println("Now a="+a);   // the reassignment works normally and here we obtain a=33 on screen
+
+
+
+// INCREASE A VARIABLE OF A CERTAIN AMOUNT WITH +=
+
+int b=5;
+b+=1;  // this command will increase b of 1
+
+System.out.println("b="+b);   // b is printed correctly as 6   OK
+
+
+
+// RESULT OF THE RATIO BETWEEN INTEGERS
+
+int g=5;
+int f=3;
+ 
+System.out.println(g/f);  // only the integer part is printed, the result is 1
+
+double h= g/f;            // this is how to declare a decimal number, the other option is float 
+
+System.out.println(h);   // the number printed is 1.0 THIS MEANS WE MUST CAST BEFORE THE RATIO
+
+double h2= (double) g/f;
+
+System.out.println(h2);    // it prints  1.6666666666666667  OK
+
+
+
+// OPERATIONS BETWEEN INTEGERS
+
+int number1= 14;
+int number2= 16;
+
+System.out.println(number1+number2);  //sum
+System.out.println(number1*number2);  // product
+System.out.println(number1/number2);  // ratio (gives only the integer part as seen above) it gives 0 correctly
+System.out.println(Math.pow(number1, number2));  // to raise number1 to the number2th  power use Math.pow(base, exponent)
+System.out.println(20%8); // it prints the remainder of the division 20:8 which is 4 OK   
+System.out.println(Math.sqrt(4)); // it prints the square root of 4 which is printed as 2.0
+
+
+
+
+
+
+// PRINTING AN INTEGER WITH %d 
+
+int income= 5000;
+
+System.out.format("I wish i earned %d $ a month\n",income);
+
+//  the %d can be mofied to diplay commas evey 3 digits
+//The %,dcode displays a decimal with commas dividing each three digits:
+
+System.out.format("I wish i earned %,d $ a month\n",income); // on my computer it actually prints 5.000, it uses the dot!
+
+// let us try to use %.d instead
+
+//System.out.format("I wish i earned %.d $ a month\n",income); IF YOU EXECUTE THIS LINE IT GIVES AN ERROR
+
+
+
+// how to use  %5d  to select only a part of a number with more that 5 digits 
+
+//int superincome= 1234567890;
+
+//System.out.format("I wish i earned %5d $ a month\n",superincome);   IT DOESN'T WORK
+
+
+
+
+
+// HOW TO DECLARE VARIABLES THAT CANNOT BE MODIFIED LATER WITH A SECOND ASSIGNMENT
+
+final int answer=42;   // final in Java is the equivalent of const in C
+
+
+System.out.format("The answer to the universe, life and it all is %d\n", answer); 
+System.out.format("Let us try to change it to 43\n"); 
+
+//answer=43;  // this command is consider illegal and the program cannot be compiled 
+// the user is reminded that " error:cannot assign a value to final variable answer"
+
+
+
+
+
+
+
+
+
+}
+
+}
